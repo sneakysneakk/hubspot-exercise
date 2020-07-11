8c6b91309156fec30c167f984d8d8b558f9f1b25
Laura Hampson <lhampson@and.digital>
2020-07-11 13:48:06 +0100

    wip applying filters

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,15 @@ import ReactDom from 'react-dom';
 
 import '../styles/index.css';
 
-console.log('App Ready');
+const getUniqueSet = (data, field) => [...new Set(data.map(obj => obj[field]))];
+
+const getGenreSet = (data) => {
+  const genreSet = new Set();
+  data.forEach(arr => 
+    arr.genre.forEach(x => genreSet.add(x))
+  );
+  return [...genreSet];
+}
 
 const Tile = ({
   content: {
@@ -21,50 +29,85 @@ const Tile = ({
   </div>
 );
 
-const Filter = ({ options }) => { 
+const Dropdown = ({ options, addFilter, field, value }) =>  { 
   return (
-  <select>
+  <select value={value} onChange={ e => addFilter(e.target.value, field)}>
+     <option
+        key="blank"
+        value=""
+      ></option>
     { options && options.map(option => (
-      <option key={option}>
+      <option
+        key={option}
+        value={option}
+      >
         {option}
       </option>
     ))}
   </select>
 )};
 
-const getUniqueSet = (data, field) => [...new Set(data.map(obj => obj[field]))];
-
-const transformData = (data) => {
-  const yearSet = getUniqueSet(data, 'year');
-  const typeSet = getUniqueSet(data, 'type');
-
-  const genreSet = new Set();
-  data.forEach(arr => 
-    arr.genre.forEach(x => genreSet.add(x))
-  );
-  return {
-    items: data,
-    years: yearSet,
-    types: typeSet,
-    genre: [...genreSet],
-  };
-}
 
 const App = () => {
-  const [ data, setData ] = useState([]);
+  const [ filteredData, setFilteredData ] = useState([]);
+  const [ originalData, setOriginalData ] = useState([]);
   const [ loading, setLoading ] = useState(true);
+  const [ years, setYears ] = useState([]);
+  const [ genres, setGenres ] = useState([]);
+  const [ filters, setFilters ] = useState({
+    year: "",
+    type: "",
+    // genre: "",
+  })
 
   useEffect(() => {
     fetch('https://hubspotwebteam.github.io/CodeExercise/src/js/data/data.json')
     .then(res => res.json())
     .then(json => {
-      setData(transformData(json.media));
+      setYears(getUniqueSet(json.media, 'year'));
+      setGenres(getGenreSet(json.media));
+      setOriginalData(json.media);
+      setFilteredData(json.media);
       setLoading(false);
     });
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    setFilteredData(applyFilters());
+  }, [filters]);
+
+  const addFilter = (filter, field) => {
+    setFilters({
+      ...filters,
+      [field]: filter,
+    });
+  };
+
+  const applyFilters = () => {
+    console.log('filters', filters);
+    const filterKeys = Object.keys(filters);
+
+    return originalData.filter(item => {
+      return filterKeys.every(key => {
+        // if (typeof filters[key] !== 'function') return true;
+        // return filters[key](item[key]);
+      })
+    })
+    // return;
+  };
+
+  const resetFilters = (e) => {
+    e.preventDefault();
+    setFilters({
+      year: "",
+      type: "",
+      // genre: "",
+    });
+  }
 
   return (
     <>
+    
       { loading && 
         <div>
           Loading
@@ -72,10 +115,39 @@ const App = () => {
       }
       { !loading && 
       <>
-        <Filter options={data.years}/>
-        <Filter options={data.genre}/>
 
-        { data && data.items && data.items.map((item) => (
+      {console.log('filtered', filteredData)}
+
+        <Dropdown
+          options={years}
+          addFilter={addFilter}
+          field="year"
+          value={filters.year}
+        />
+        <Dropdown
+          options={genres}
+          addFilter={addFilter}
+          field="genre"
+          value={filters.genre}
+        />
+
+        <button onClick={(e=>resetFilters(e))}>Clear filters</button>
+        
+        {/* { data.types.map(x => (
+          <>
+            <input type="radio" name="type" value={x} />
+            <label>{x}</label>
+          </>
+        ))} */}
+        <div onChange={(e)=> addFilter(e.target.value, 'type')}>
+          <input id="bookradio" type="radio" name="type" value="book" />
+          <label htmlFor="bookradio">Books</label>
+          <input id="movieradio" type="radio" name="type" value="movie" />
+          <label htmlFor="movieradio">Movies</label>
+        </div>
+        
+
+        { filteredData && filteredData.map((item) => (
           <Tile
             key={item.title}
             content={item}
--- submission.md
+++ submission.md
@@ -9,11 +9,8 @@ _your answer here_
 Added some more checks for clean data - being able to see the data I knew it was all clean but irl I would put in some checks for dodgy data 
 
 
-Woudln't like to manipulate the data that much client side 
+Wouldn't like to manipulate the data that much client side 
 
-I've hardcoded the genres and types into the filters 
-- I would hope that I can get these from the API 
-- I could have filtered the data to retrieve these dynamically but this would not be very scalable if you had thousands of items
 
 ## How did you deviate from the directions, if at all, and why?
 
@@ -28,3 +25,7 @@ but that seems gross and unrealistic
 ## Is there anything else you'd like to let us know?
 
 _your answer here_
+
+Filtered dynamically so that API changes wouldn't break features
+
+Originally did one state object with all needed but broke into multiple states 
