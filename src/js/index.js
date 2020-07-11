156aa6f2338dffd83f3e6233301f280ccb8e4154
Laura Hampson <lhampson@and.digital>
2020-07-11 09:59:18 +0100

    create data sets for filters

--- src/js/index.js
+++ src/js/index.js
@@ -10,40 +10,80 @@ const Tile = ({
     poster,
     title,
     year,
+    genre,
   }}) => (
   <div className="tile">
     <img src={poster} />
     <div>{`${title} (${year})`}</div>
-    <div>Genres: </div>
+    <div>Genres: 
+      { genre }
+    </div>
   </div>
 );
 
-const Filter = () => (
+const Filter = ({ options }) => { 
+  return (
   <select>
-    <option>Option 1</option>
+    { options && options.map(option => (
+      <option key={option}>
+        {option}
+      </option>
+    ))}
   </select>
-);
+)};
+
+const getUniqueSet = (data, field) => [...new Set(data.map(obj => obj[field]))];
+
+const transformData = (data) => {
+  const yearSet = getUniqueSet(data, 'year');
+  const typeSet = getUniqueSet(data, 'type');
+
+  const genreSet = new Set();
+  data.forEach(arr => 
+    arr.genre.forEach(x => genreSet.add(x))
+  );
+  return {
+    items: data,
+    years: yearSet,
+    types: typeSet,
+    genre: [...genreSet],
+  };
+}
 
 const App = () => {
   const [ data, setData ] = useState([]);
+  const [ loading, setLoading ] = useState(true);
 
   useEffect(() => {
     fetch('https://hubspotwebteam.github.io/CodeExercise/src/js/data/data.json')
     .then(res => res.json())
-    .then(json => setData(json.media));
+    .then(json => {
+      setData(transformData(json.media));
+      setLoading(false);
+    });
   }, [])
 
   return (
     <>
-      <Filter />
-      <Filter />
-      {console.log(data)}
+      { loading && 
+        <div>
+          Loading
+        </div>
+      }
+      { !loading && 
+      <>
+        <Filter options={data.years}/>
+        <Filter options={data.genre}/>
 
-      { data && data.map((item) => (
-        <Tile
-          content={item}
-        />
-      ))}
+        { data && data.items && data.items.map((item) => (
+          <Tile
+            key={item.title}
+            content={item}
+          />
+        ))}
+      </>
+      }
+     
     </>
   );
 };
--- submission.md
+++ submission.md
@@ -6,9 +6,24 @@ These notes will be read by HubSpot developers. Drop us a line!
 
 _your answer here_
 
+Added some more checks for clean data - being able to see the data I knew it was all clean but irl I would put in some checks for dodgy data 
+
+
+Woudln't like to manipulate the data that much client side 
+
+I've hardcoded the genres and types into the filters 
+- I would hope that I can get these from the API 
+- I could have filtered the data to retrieve these dynamically but this would not be very scalable if you had thousands of items
+
 ## How did you deviate from the directions, if at all, and why?
 
 _your answer here_
+
+Added a class for the first exercise - if I were to do it without adding the class I would : 
+
+but that seems gross and unrealistic 
+
+
 
 ## Is there anything else you'd like to let us know?
 
